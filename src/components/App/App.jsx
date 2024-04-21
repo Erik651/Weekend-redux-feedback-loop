66dae221e3b1fe0bfbabd6189057665f3de9ce04
Erik Silcox <eriksilcox651@gmail.com>
2024-04-21 13:00:26 -0500

    added component imports to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import BeingSupported from '../BeingSupported/BeingSupported.jsx';
+import FeelingToday from '../FeelingToday/FeelingToday.jsx';
+import UnderstandingContent from '../UnderstandingContent/UnderstandingContent';
+import SubmissionSuccess from '../SubmissionSubmit/SubmissionSuccess';
+import ReviewFeedback from '../ReviewFeedback/ReviewFeedback';
+import Header from '../Header/Header.js';
+import ReviewFeedback from '../ReviewFeedback/ReviewFeedback';
+
 
 function App() {
 
@@ -8,6 +16,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
+        
         <h4>Don't forget it!</h4>
       </header>
     </div>
