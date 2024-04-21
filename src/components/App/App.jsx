c127e56eef4f141e9eb61853273e85fe8ccab69a
Erik Silcox <eriksilcox651@gmail.com>
2024-04-21 15:18:56 -0500

    added Route paths to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import BeingSupported from '../BeingSupported/BeingSupported.jsx';
 import FeelingToday from '../FeelingToday/FeelingToday.jsx';
@@ -7,16 +8,30 @@ import UnderstandingContent from '../UnderstandingContent/UnderstandingContent';
 import SubmissionSuccess from '../SubmissionSubmit/SubmissionSuccess';
 import ReviewFeedback from '../ReviewFeedback/ReviewFeedback';
 import Header from '../Header/Header.js';
-import ReviewFeedback from '../ReviewFeedback/ReviewFeedback';
-
+import { Router } from 'express';
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Feedback!</h1>
+        <Router>
+          <Route exact path="/beingSupported">
+            <BeingSupported />
+          </Route>
+          <Route exact path="/feelingToday">
+            <FeelingToday />
+          </Route>
+          <Route exact path="understandingContent">
+            <UnderstandingContent />
+          </Route>
+          <Route exact path="submissionSuccess">
+            <SubmissionSuccess />
+          </Route>
+          <Route exact path="reviewFeedback">
+            <ReviewFeedback />
+          </Route>
+        </Router>
         <h4>Don't forget it!</h4>
       </header>
     </div>
