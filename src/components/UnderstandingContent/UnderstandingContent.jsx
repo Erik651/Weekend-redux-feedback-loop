ab22d4309f5f2bae6794a64b9c4cc90a7e10b65e
Erik Silcox <eriksilcox651@gmail.com>
2024-04-21 22:59:53 -0500

    added useHistory/handleClick next button to UnderstandingContent.jsx

--- src/components/UnderstandingContent/UnderstandingContent.jsx
+++ src/components/UnderstandingContent/UnderstandingContent.jsx
@@ -1,10 +1,20 @@
+import React from "react";
+import { useHistory } from 'react-router-dom';
+
+
 function UnderstandingContent() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    alert('Headed to Being Supported page');
+    history.push('/beingSupported')
+  }
 return (
   <section>
 <h2>How well are you understanding the content?</h2>
-<button>Next</button>
 <input>Understanding?</input>
 
+<button onClick={handleClick}>Next</button>
   </section>
 )
 }
