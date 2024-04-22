686e32dcf1d95250e5f3003887ab34a67051bb5d
Erik Silcox <eriksilcox651@gmail.com>
2024-04-22 09:51:56 -0500

    added useState to UnderstandingContent.jsx

--- src/components/UnderstandingContent/UnderstandingContent.jsx
+++ src/components/UnderstandingContent/UnderstandingContent.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
-
+import { useDispatch } from "react-redux";
 
 function UnderstandingContent() {
+  const [score, setScore] = useState('');
   const history = useHistory();
+  const dispatch = useDispatch();
 
-  const handleClick = () => {
+  const handleClick = (event) => {
+    console.log(`adding understanding score`, { score });
+
+    let action = {
+      payload: { score: score }
+    };
+
+    dispatch(action);
+
     alert('Headed to Being Supported page');
     history.push('/support')
   }
 return (
   <section>
 <h2>How well are you understanding the content?</h2>
-<input>Understanding?</input>
+<input
+required
+placeholder="Understanding 1-5"
+value={score}
+onChange={(event) => setScore(event.target.value)}/>Understanding?
 
 <button onClick={handleClick}>Next</button>
   </section>
