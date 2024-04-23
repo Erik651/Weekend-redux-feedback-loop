bd3ef76d5303fc72566bee67becdf430d931eeb2
Erik Silcox <eriksilcox651@gmail.com>
2024-04-23 00:26:11 -0500

    added leaveComment,feelingToday,beingSupported,understandingContent reducers to store.js

--- src/store.js
+++ src/store.js
@@ -11,11 +11,42 @@ const feedbackList = (state = [], action) => {
   return state;
 }
 
+const leaveComment = (state = '', action) => {
+  if (action.type === 'ADD_COMMENTS') {
+    return action.payload;
+  }
+  return state;
+}
+
+const feelingToday = (state = 0, action) => {
+  if(action.type === 'ADD_FEELING_SCORE') {
+    return action.payload;
+  }
+  return state;
+}
+
+const beingSupported = (state = 0, action) => {
+  if (action.type === 'ADD_SUPPORTED_SCORE') {
+    return action.payload;
+  }
+  return state;
+}
+
+const understandingContent = (state = 0, action) => {
+  if (action.type === 'ADD_UNDERSTANDING_SCORE') {
+    return action.payload;
+  }
+  return state;
+}
+
+
+
 // Step 3: Create store
 const reduxStore = createStore(
   combineReducers({
     // reducers go here
-    feedbackList
+    feedbackList,
+    leaveComment,
   }),
   applyMiddleware(logger)
 );
